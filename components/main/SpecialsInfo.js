5a1fcd3571341e111035a53cc15803a27bf189dd
Alexey Semka <vorzelski@gmail.com>
2021-11-13 21:47:17 +0200

    new commit

--- components/main/SpecialsInfo.js
+++ components/main/SpecialsInfo.js
@@ -4,20 +4,43 @@ import Image from 'next/image'
 
 import classes from '../../styles/main/specials.module.scss'
 
-const SpecialsInfo = ({specials}) => {
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
-    return <div className={classes.productsSpecialsWrap}>
-        { specials.map(special => {
-            return <div key={special.product_id} className={classes.productsSpecials}>       
-                <Link href={`/product/${special.url}`} as={`/product/${special.url}`}><a><Image src={special.image} alt={special.name} className={classes.productsSpecials__img} width="500px" height="500px" /></a></Link>
-                <Link href={`/product/${special.url}`} as={`/product/${special.url}`}><a><h2 className={classes.productsSpecials__name}>{special.name}</h2></a></Link>
-                <p>{special.price} USD</p>
-                <p>{special.description}</p>
-            </div>
-         })
+const SpecialsInfo = ({specials}) => {
+    let slidesCount 
+    let pageWidthValue
+    if (typeof window !== 'undefined') {
+    pageWidthValue = window.innerWidth
         }
-           </div>
-   
+
+if(pageWidthValue > 768){
+    return <div className={classes.productsSpecialsWrap}>
+    { specials.map(special => {
+        return <div key={special.product_id} className={classes.productsSpecials}>       
+            <Link href={`/product/${special.url}`} as={`/product/${special.url}`}><a><Image src={special.image} alt={special.name} className={classes.productsSpecials__img} width="500px" height="500px" /></a></Link>
+            <Link href={`/product/${special.url}`} as={`/product/${special.url}`}><a><h2 className={classes.productsSpecials__name}>{special.name}</h2></a></Link>
+            <p>{special.price} USD</p>
+            <p>{special.description}</p>
+        </div>
+     })
+    }
+       </div>
+}else{
+    return <Carousel className={classes.productsSpecialsWrap}>
+    { specials.map(special => {
+        return <div key={special.product_id} className={classes.productsSpecials}>       
+            <Link href={`/product/${special.url}`} as={`/product/${special.url}`}><a><Image src={special.image} alt={special.name} className={classes.productsSpecials__img} width="500px" height="500px" /></a></Link>
+            <Link href={`/product/${special.url}`} as={`/product/${special.url}`}><a><h2 className={classes.productsSpecials__name}>{special.name}</h2></a></Link>
+            <p>{special.price} USD</p>
+            <p>{special.description}</p>
+        </div>
+     })
+    }
+       </Carousel>
+
+}
+      
 }
     
 export default SpecialsInfo
--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
 <link href="https://fonts.googleapis.com/css2?family=Newsreader:wght@500&display=swap" rel="stylesheet"/>   
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"/>  
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"/>   
         </Head>
         <body>
           <Main />
