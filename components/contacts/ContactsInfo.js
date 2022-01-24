4bd820c040439bc046014aa1de07b3c614a3f959
Alexey Semka <vorzelski@gmail.com>
2022-01-24 06:15:40 +0200

    scss commit

--- components/contacts/ContactsInfo.js
+++ components/contacts/ContactsInfo.js
@@ -10,9 +10,9 @@ const ContactsInfo = ({contacts}) => {
     return <div key={contact.id} className={classes.contactsPageLeft}>
         <h2>{contact.title}</h2>
     <p>{contact.description}</p>
-    <p><Link href={"mailto:" + contact.email} as={"mailto:" + contact.email}><a><i aria-hidden="true" className="fa fa-envelope"></i>{contact.email}</a></Link></p>
-    <p><Link href={"tel:" + contact.phone} as={"tel:" + contact.phone}><a><i aria-hidden="true" className="fa fa-phone"></i>{contact.phone}</a></Link></p>
-    <p><Link target="_blank" rel="noopener noreferrer" href={contact.map_link} as={contact.map_link}><a><i aria-hidden="true" className="fa fa-map-marker"></i>{contact.address}</a></Link></p>
+    <p className={classes.contactsMail}><Link href={"mailto:" + contact.email} as={"mailto:" + contact.email}><a><i aria-hidden="true" className="fa fa-envelope"></i>{contact.email}</a></Link></p>
+    <p className={classes.contactsPhone}><Link href={"tel:" + contact.phone} as={"tel:" + contact.phone}><a><i aria-hidden="true" className="fa fa-phone"></i>{contact.phone}</a></Link></p>
+    <p className={classes.contactsMap}><Link target="_blank" rel="noopener noreferrer" href={contact.map_link} as={contact.map_link}><a><i aria-hidden="true" className="fa fa-map-marker"></i>{contact.address}</a></Link></p>
     </div>
     }) 
     } 
